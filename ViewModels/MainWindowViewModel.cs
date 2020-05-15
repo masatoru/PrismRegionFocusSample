db407efe3e4f7e37e0c14342486f570440d51f2f
Satoru Fujimori <satoru.fujimori@gmail.com>
2020-05-15 14:49:44 +0900

    Prism Applicationに作り替える

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace PrismRegionFocusSample.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Region Focus Sample";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
